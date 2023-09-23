77e28bb7cb8d67e70c5df9b835c3a2a459b22df3
brendan.craven <brendan.craven@student.howest.be>
2023-09-23 20:42:47 +0200

    refactor util methods to its own file

--- parser_engine/util.py
+++ parser_engine/util.py
@@ -1,6 +1,12 @@
 from common.constants import DEV_MODE
+from tables import phoneme_flags
+from util.util import matches_bitmask
 
 
 def dev_print(message):
     if DEV_MODE:
         print(message)
+
+
+def phoneme_has_flag(phoneme, flag):
+    return matches_bitmask(phoneme_flags[phoneme], flag)
