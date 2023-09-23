4c55d48e614d20665d2d6a3638996d9486268244
brendan.craven <brendan.craven@student.howest.be>
2023-09-23 12:07:01 +0200

    massive comment explaining the parser method

--- parser_engine/p-conv/parser.py
+++ parser_engine/p-conv/parser.py
@@ -62,9 +62,53 @@ def wild_match(sign1):
 
 def parser1(input_data):
     """
-    Parse an input array of phonemes and stress markers
+    Parse an input array of phonemes and stress markers.
 
-    :param input_data: The input array of phonemes and stress markers
+    The input[] buffer contains a string of phonemes and stress markers along the lines of:
+        DHAX KAET IHZ AH5GLIY. <0x9B>
+
+    The byte 0x9B marks the end of the buffer.
+
+    Some phonemes are 2 bytes long, such as "DH" and "AX". Others are 1 byte long, such as "T" and "Z".
+    There are also stress markers, such as "5" and ".".
+
+    The first character of the phonemes are stored in the table sign_input_table1[]
+    The second character of the phonemes are stored in the table sign_input_table2[]
+    The stress characters are arranged in low to high stress order in stress_input_table[]
+
+    The following process is used to parse the input[] buffer:
+
+    Repeat until the <0x9B> character:
+        First, a search is made for a 2 character match to phonemes that do not end with the '*' (wildcard) character.
+        On a match, the index of the phoneme is added to phoneme_index[] and the buffer position is advanced 2 bytes.
+
+        If this fails, a search is made for a 1 character match against all phonemes ending with a '*'. If this
+        succeeds, the phoneme is added to phoneme_index[] and the buffer position is advanced 1 byte.
+
+        If this fails, search for a 1 character match in the stress_input_table[]. If this succeeds, the stress value
+        is placed in the last stress[] table at the same index of the last added phoneme, and the buffer position is
+        advanced by 1 byte.
+
+        If this fails, return a 0.
+
+    On success:
+        1. phoneme_index[] will contain the index of all the phonemes.
+        2. The last index in phoneme_index will be 255.
+        3. stress[] will contain the stress value for each phoneme
+
+    input[] holds the string of phonemes, each two bytes wide
+    sign_input_table1[] holds the first character of each phoneme
+    sign_input_table2[] holds the second character of each phoneme
+    phoneme_index[] holds the indexes of the phonemes after parsing input[]
+
+    The parser scans through the input[], finding the names of the phonemes by searching sign_input_table1[] and
+    sign_input_table2[]. On a match, it copies the index of the phoneme into the phoneme_index_table[]
+
+    The character <0x9B> marks the end of text in input[]. When it is reached, the index 255 is placed at the end of
+    the phoneme_index_table[], and the function returns with a 1 indicating success
+
+    :param input_data: The input array of phonemes and stress markers.
+                       Consists of phoneme_index[] and stress[], bothUint8Arrays
     :return: A dictionary containing 'phoneme_index' and 'stress' arrays, or None on failure
     """
     phoneme_index = bytearray(256)
@@ -110,16 +154,3 @@ def parser1(input_data):
     return {'phoneme_index': phoneme_index, 'stress': stress}
 
 
-# Example usage:
-input_data = bytearray([
-    0x44, 0x48, 0x41, 0x58, 0x20, 0x4B,
-    0x41, 0x45, 0x54, 0x20, 0x49, 0x48,
-    0x5A, 0x20, 0x41, 0x48, 0x35, 0x47,
-    0x4C, 0x49, 0x59, 0x2E, 0x20, 0x9B
- ])
-result = parser1(input_data)
-if result:
-    print(result['phoneme_index'])
-    print(result['stress'])
-else:
-    print("Parsing failed.")
